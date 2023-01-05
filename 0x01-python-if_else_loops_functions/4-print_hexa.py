a4c30ed46ec1e0c982c0ebc715829693d9d029d6
victornwaonicha <victornwaonicha@gmail.com>
2023-01-04 16:34:44 -0800

    Python files

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print("{:02d}, ".format(i), end='')
-    else:
-        print("{:02d}".format(i))
+for i in range(0, 99):
+    print("{0:d} = 0x{0:x}".format(i))
