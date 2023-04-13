ae14e2080832dfe602a6517cc035df8dd76b847e
victornwaonicha <victornwaonicha@gmail.com>
2023-04-13 08:46:43 -0700

    Files Made Executable

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const x = process.argv[2];
 
-if (!paeseInt(x)) {
-	console.log('Missing number of occurrences');
+if (!parseInt(x)) {
+  console.log('Missing number of occurrences');
 } else {
-	for (let i = 0; i< x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
